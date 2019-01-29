b71ef8ef6e659f911981a4e219afbbbc3af603ef
luo3300612 <591486669@qq.com>
2019-01-29 19:14:40 +0800

    add flask-script

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -13,4 +15,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    manager.run()

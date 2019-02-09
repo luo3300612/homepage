272af26ac87bdd8fe03f734594aa562040b0c878
luo3300612 <591486669@qq.com>
2019-02-09 22:11:43 +0800

    update

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN") or "john@example.com"
     FLASKY_POSTS_PER_PAGE = 5
     FLASKY_FOLLOWERS_PER_PAGE = 5
+    FLASKY_COMMENTS_PER_PAGE = 5
 
     @staticmethod
     def init_app(app):
